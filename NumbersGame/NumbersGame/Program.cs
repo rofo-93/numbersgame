1ce4019a6e3073bca23afe90c2d19350f04e3258
rofo-93 <robin_forsberg93@hotmail.com>
2026-09-13 13:15:31 +0200

    Add files via upload

---
+++ NumbersGame/NumbersGame/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace NumbersGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök.");
+            Random random = new Random();
+            int number = random.Next(1, 20);
+            bool CheckGuess(int guesser, int number)
+            {
+                return guesser == number;
+            }
+
+            for (int n = 0; n < 5; n++)
+            {
+                Console.WriteLine("Gissa ett tal:");
+                int guesser = Convert.ToInt32(Console.ReadLine());
+
+                if (CheckGuess(guesser, number))
+                {
+                    Console.WriteLine("Wohoo! Du klarade det!");
+                    return;
+                }
+
+                if (guesser < number)
+                {
+                    Console.WriteLine("Tyvärr, du gissade för lågt!");
+                }
+                else
+                {
+                    Console.WriteLine("Tyvärr, du gissade för högt!");
+                }
+
+
+            }
+
+
+
+
+
+        }
+    }
+}
